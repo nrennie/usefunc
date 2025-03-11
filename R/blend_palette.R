b1697fa98a9cad53244b076ba442e85528e8bd65
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-03-11 15:57:59 +0000

    line length

--- R/blend_palette.R
+++ R/blend_palette.R
@@ -2,8 +2,10 @@
 #'
 #' This function allows users to blend a colour palette vector with another
 #' colour to make a more cohesive palette.
-#' @param palette The starting vector for the palette, which must be either be recognised colour names (e.g. "blue") or hex colour codes (e.g. "#ffffff").
-#' @param blend_colour The colour to blend with, which must be either be a recognised colour name (e.g. "blue") or a hex colour code (e.g. "#ffffff").
+#' @param palette The starting vector for the palette, which must be either be 
+#' recognised colour names (e.g. "blue") or hex colour codes (e.g. "#ffffff").
+#' @param blend_colour The colour to blend with, which must be either be a 
+#' recognised colour name (e.g. "blue") or a hex colour code (e.g. "#ffffff").
 #' @param view_palette Boolean whether or not to view the palette.
 #' @return A character vector of colours.
 #' @export
