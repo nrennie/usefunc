be785bc71fa262a28064017b44348471f855b90c
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-11-09 20:55:26 +0000

    update template defaults

--- inst/tt-template.R
+++ inst/tt-template.R
@@ -1,4 +1,3 @@
-
 # Load packages -----------------------------------------------------------
 
 library(tidyverse)
@@ -17,19 +16,19 @@ tuesdata <- tidytuesdayR::tt_load(date_chr)
 
 # Load fonts --------------------------------------------------------------
 
-font_add_google("Roboto", "roboto")
+font_add_google("Oswald")
+font_add_google("Nunito")
 showtext_auto()
 showtext_opts(dpi = 300)
+title_font <- "Oswald"
+body_font <- "Nunito"
 
 
 # Define colours and fonts-------------------------------------------------
 
-bg_col <- ""
-text_col <- ""
-highlight_col <- ""
-
-body_font <- "roboto"
-title_font <- "roboto"
+bg_col <- "#F2F4F8"
+text_col <- "#151C28"
+highlight_col <- "#7F055F"
 
 
 # Data wrangling ----------------------------------------------------------
@@ -65,37 +64,43 @@ cap <- paste0(
 
 # Plot --------------------------------------------------------------------
 
-
-theme(
-  plot.margin = margin(5, 5, 5, 5),
-  plot.title.position = "plot",
-  plot.caption.position = "plot",
-  plot.background = element_rect(fill = bg_col, colour = bg_col),
-  panel.background = element_rect(fill = bg_col, colour = bg_col),
-  plot.title = element_textbox_simple(
-    colour = text_col,
-    hjust = 0.5,
-    halign = 0.5,
-    margin = margin(b = 5, t = 5),
-    family = title_font,
-    face = "bold",
-    size = rel(1.8)
-  ),
-  plot.subtitle = element_textbox_simple(
-    colour = text_col,
-    hjust = 0.5,
-    halign = 0.5,
-    margin = margin(b = 5, t = 5),
-    family = body_font
-  ),
-  plot.caption = element_textbox_simple(
-    colour = text_col,
-    hjust = 0.5,
-    halign = 0.5,
-    margin = margin(b = 5, t = 5),
-    family = body_font
+theme_minimal(base_size = 10, base_family = body_font) +
+  theme(
+    plot.margin = margin(5, 5, 5, 5),
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.background = element_rect(fill = bg_col, colour = bg_col),
+    panel.background = element_rect(fill = bg_col, colour = bg_col),
+    plot.title = element_textbox_simple(
+      colour = text_col,
+      hjust = 0,
+      halign = 0,
+      margin = margin(b = 5, t = 5),
+      family = title_font,
+      face = "bold",
+      size = rel(1.5)
+    ),
+    plot.subtitle = element_textbox_simple(
+      colour = text_col,
+      hjust = 0.5,
+      halign = 0.5,
+      margin = margin(b = 5, t = 5),
+      family = body_font
+    ),
+    plot.caption = element_textbox_simple(
+      colour = text_col,
+      hjust = 0,
+      halign = 0,
+      margin = margin(b = 0, t = 10),
+      family = body_font
+    ),
+    strip.text = element_textbox_simple(
+      face = "bold",
+      margin = margin(t = 10),
+      size = rel(0.9)
+    ),
+    panel.grid.minor = element_blank()
   )
-)
 
 
 # Save gif ----------------------------------------------------------------
